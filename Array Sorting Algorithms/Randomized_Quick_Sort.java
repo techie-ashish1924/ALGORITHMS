c4949ffc16b0f41be2069a4a1cdfbce4cddc5918
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-09 15:04:12 +0530

    final commit

---
+++ Array Sorting Algorithms/Randomized_Quick_Sort.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Randomized_Quick_Sort {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+    
+}
